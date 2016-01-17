7ce0e6b13d261a8e9631b1f00bc18e309ff83fb6
betahikaru <beta.hikaru2000+github@gmail.com>
2016-01-17 17:10:30 +0900

    Initial commit

---
+++ app/asset/js/app.js
@@ -0,0 +1,60 @@
+// Application
+App = new Backbone.Marionette.Application();
+
+// Event
+//   Usage: App.vent.trigger("someEvent", "a");
+App.vent.on("someEvent", function(someData){
+  alert("Received", someData);
+});
+
+// Regions
+App.addRegions({
+  mainRegion: "#main-content",
+  navigationRegion: "#navigation"
+});
+
+// Models
+TodoModel = Backbone.Model.extend({
+  defaults: {
+    "description": "",
+    "done": false
+  }
+});
+TodoCollection = Backbone.Collection.extend({
+  model: TodoModel
+});
+
+// Views
+TodoNaviView = Backbone.Marionette.ItemView.extend({
+  template: "#todo-navigation-template"
+});
+TodoView = Backbone.Marionette.ItemView.extend({
+  template: "#todo-template",
+  ui: {
+    checkbox: "input[type=checkbox]"
+  },
+  onRender: function() {
+    // Update checked property
+    var doned = this.model.get('done');
+    this.ui.checkbox.prop('checked', doned);
+  }
+});
+TodoListView = Backbone.Marionette.CollectionView.extend({
+  itemView: TodoView
+});
+
+// Application
+var todoNaviView = new TodoNaviView();
+var todo1 = new TodoModel({
+  description: "TODO表示機能",
+  done: true
+});
+var todo2 = new TodoModel({
+  description: "TODO追加機能"
+});
+var todoCollection = new TodoCollection([todo1, todo2]);
+var todoListView = new TodoListView({
+  collection: todoCollection
+});
+App.navigationRegion.show(todoNaviView);
+App.mainRegion.show(todoListView);
