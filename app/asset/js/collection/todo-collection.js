1758030d2611e16d142eb74f2616bdc7f6c84e9a
betahikaru <beta.hikaru2000+github@gmail.com>
2016-01-19 02:43:26 +0900

    Create Todo

--- app/asset/js/collection/todo-collection.js
+++ app/asset/js/collection/todo-collection.js
@@ -1,3 +1,9 @@
 App.TodoCollection = Backbone.Collection.extend({
-  model: App.TodoModel
+  model: App.TodoModel,
+  initialize: function(models, options) {
+    this.listenTo(App.vent, "todo:create", this.onCreateTodo);
+  },
+  onCreateTodo: function(todoModel) {
+    this.add(todoModel);
+  }
 });
--- app/asset/js/view/todo-navi-view.js
+++ app/asset/js/view/todo-navi-view.js
@@ -1,3 +1,17 @@
 App.TodoNaviView = Backbone.Marionette.ItemView.extend({
-  template: "#todo-navigation-template"
+  template: "#todo-navigation-template",
+  ui: {
+    "create_button": "button",
+    "todo_desc_text": "#inputTodoDesc"
+  },
+  events: {
+    "click @ui.create_button": "createTodo"
+  },
+  createTodo: function() {
+    var todoDesc = this.ui.todo_desc_text.val();
+    var todoModel = new App.TodoModel({
+      description: todoDesc
+    });
+    App.vent.trigger("todo:create", todoModel);
+  }
 });
